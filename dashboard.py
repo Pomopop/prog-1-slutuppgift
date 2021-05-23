730b5d864f91a2805c0614a9e7441203bb275d56
Pomopop <70264384+Pomopop@users.noreply.github.com>
2021-05-23 20:20:45 +0200

    kjj

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,4 @@
+
 import plotly.express as px 
 import dash
 import dash_core_components as dcc
@@ -8,90 +9,104 @@ from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 from operator import truediv
 
+
 app = dash.Dash(__name__)
 
-# genererar mockup data
-DBAG = pd.read_csv("deathbyagegroup.csv",)
-GD = pd.read_csv("Gender_Data.csv")
+DBAG = pd.read_csv("deathbyagegroup.csv",) #Läser in filen deathbyagegroup så man kan jobba med den
+GD = pd.read_csv("Gender_Data.csv") #läser in filen gender_data så man kan jobba med den.
 
-Åldersgrupper = ["0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89", "90+"] 
+
+#får fram alla data så att den kan läggas i data framen
+Åldersgrupper = ["0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89", "90+"]
+
 Dött = []
-Sjuka = []
-
 for x in Åldersgrupper:
     DBAGgrupper = DBAG[DBAG["Age_Group"] == x]   
     dfsm = DBAGgrupper.sum()
     Döda = dfsm["Total_Deaths"]
     Dött.extend([Döda])
 
+
+Sjuka = []
 for x in Åldersgrupper:
     DBAGgrupper = DBAG[DBAG["Age_Group"] == x]   
     dfsm = DBAGgrupper.sum()
     Fall = dfsm["Total_Cases"]
     Sjuka.extend([Fall])
 
-
 SjukaperDöd= list(map(truediv, Sjuka, Dött))
-
 
 under60 = Dött[:-4]
 över60 = Dött[6:]
-
 döttunder60 = sum(under60)
 döttöver60 = sum(över60)
+Dödöverunder60 = [döttunder60,döttöver60]
+#Slut
 
-DöttTotal = [döttunder60,döttöver60]
-Åldersgrupper2 = ["0-59", "60-90+"]
 
-#df_TE19 = pd.DataFrame({"Närvaro":TE19})
-#df_NA19 = pd.DataFrame({"Närvaro":NA19})
+df = pd.DataFrame({
+    "ÅldersGrupper": ["0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89", "90+"],
+    "ÅldersGrupper2": ["0-59", "60-90+"],
+    "Döda": Dött,
+    "FallperÅldersgrupp": Sjuka,
+    "SjukaperDöd": SjukaperDöd,
+    "dödöverunder60": Dödöverunder60
+})
 
-# skapa fig
+figpiedöd = px.pie(df, X="ÅldersGrupper", Y="Döda", title= "Döda per åldersgrupp")
+figpiedödöverunder60 = px.pie(df, X="ÅldersGrupper", Y="Dödöverunder60", title= "Döda över och under 60")
 
-grafdöda = px.pie(values = Dött, names = Åldersgrupper, title= "Antalet döda i varje åldergrupp")
+figbarsjuka = go.Figure(
+   data = [go.Bar(y="FallperÅldersgrupp", x="ÅldersGrupper")],
+    layout_title_text="Antalet Fall",
 
-grafsjuka = go.Figure(
-    data = [go.Bar(y=Sjuka, x=Åldersgrupper)],
-    layout_title_text="Antalet Fall"
+figbarsjukaperdöd = go.Figure(
+    data = [go.Bar(y="SjukaperDöd", x="ÅldersGrupper")],
+    layout_title_text="Hur många som överlever per död"
+
+    )
 )
 
-grafleverperfall = go.Figure(
-    data = [go.Bar(y=SjukaperDöd, x=Åldersgrupper)],
-    layout_title_text="Hur många som många som överlever per fall"
-)
 
-graföverunder60 = px.pie(values=DöttTotal, names=Åldersgrupper2, title="döda över och under 60")
-
-
-# utseendet
 app.layout = HTML.Div(children=[
-    HTML.H1(children = "Närvarograd för olika klasser"), 
-
+    HTML.H1(children = "Covid statisik på olika Åldersgrupper"), 
+    
+    HTML.p("Covid data gällande olika åldersgrupper"),
     dcc.Dropdown(
         id = "drop",
-        options = [dict(label = "TE19", value="TE19"), dict(label = "NA19", value="NA19")],
-        value="TE19"
+        value="x",
+        options=[{'value': x, 'label': x} 
+                for x in ['Döda', 'Sjuka', 'Sjuka per död', 'Döda över och under 60']],
+        clearable=False
+        
     ),
 
-    dcc.Graph(
-        id = "graph",
-        figure = grafdöda)
-
-
+        dcc.Graph(
+            id = "graph",
+            figure = figpiedöd
+        )
 ])
+
 
 @app.callback(
     Output("graph", "figure"),
-    [Input("drop", "value")]
+    [Input("drop",)]
 )
+
 def update_figure(value):
-#    if value == "TE19": df =df_TE19
-#    elif value == "NA19": df = df_NA19
+    
+    if x == 'Döda': fig = figpiedöd
+    elif x == 'Sjuka': fig = figbarsjuka
+    elif x == 'Döda över och under 60': fig = figpiedödöverunder60
 
-
-    grafleverperfall.update_layout(transition_duration=500)
-    grafdöda = px.pie(values = Dött, names = Åldersgrupper, title= "Antalet döda i varje åldergrupp")
-    return grafdöda
+    fig.update_layout(transition_duration=500)
+    return fig
 
 if __name__ == "__main__":
     app.run_server(debug = True)
+
+
+
+
+
+
